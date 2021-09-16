44d5d6446d0f3abdc509cfd2a13e635ddf8b7396
zrji <zrji@zrji.com>
2021-09-16 15:01:44 +0800

    添加侧边栏收缩

--- main-vue/src/layout/index.vue
+++ main-vue/src/layout/index.vue
@@ -1,47 +1,50 @@
 <template>
     <div class="home-page">
-        <el-row class="tac">
-            <el-col :span="3">
-                <el-menu
-                    class="el-menu-vertical-demo"
-                    :default-active="activeRoute"
-                    @select="handleSelect"
-                    background-color="#545c64"
-                    text-color="#fff"
-                    active-text-color="#ffd04b"
-                >
-                    <el-menu-item index="Main">
-                        <i class="el-icon-ship"></i>
-                        <span>Vue主应用</span>
-                    </el-menu-item>
-                    <el-menu-item index="Vue">
-                        <i class="el-icon-basketball"></i>
-                        <span slot="title">Vue子应用</span>
-                    </el-menu-item>
-                    <el-menu-item index="React">
-                        <i class="el-icon-soccer"></i>
-                        <span slot="title">React子应用</span>
-                    </el-menu-item>
-                    <el-menu-item index="Vue3">
-                        <i class="el-icon-basketball"></i>
-                        <span slot="title">Vue3(vite+ts)</span>
-                    </el-menu-item>
-                    <el-menu-item index="Purehtml">
-                        <i class="el-icon-football"></i>
-                        <span slot="title">纯HTML</span>
-                    </el-menu-item>
-                    <el-menu-item index="Umi">
-                        <i class="el-icon-soccer"></i>
-                        <span slot="title">Umi子应用</span>
-                    </el-menu-item>
-                </el-menu>
-            </el-col>
-            <el-col :span="21">
-                <router-view id="MainAppArea" v-if="$route.name === 'Main'" />
+        <div class="left-wrap">
+            <el-menu
+                class="el-menu-vertical-demo"
+                :collapse="isCollapse"
+                :default-active="activeRoute"
+                @select="handleSelect"
+                background-color="#545c64"
+                text-color="#fff"
+                active-text-color="#ffd04b"
+            >
+                <el-menu-item index="Main">
+                    <i class="el-icon-ship"></i>
+                    <span>Vue主应用</span>
+                </el-menu-item>
+                <el-menu-item index="Vue">
+                    <i class="el-icon-basketball"></i>
+                    <span slot="title">Vue子应用</span>
+                </el-menu-item>
+                <el-menu-item index="React">
+                    <i class="el-icon-soccer"></i>
+                    <span slot="title">React子应用</span>
+                </el-menu-item>
+                <el-menu-item index="Vue3">
+                    <i class="el-icon-basketball"></i>
+                    <span slot="title">Vue3(vite+ts)</span>
+                </el-menu-item>
+                <el-menu-item index="Purehtml">
+                    <i class="el-icon-football"></i>
+                    <span slot="title">纯HTML</span>
+                </el-menu-item>
+                <el-menu-item index="Umi">
+                    <i class="el-icon-soccer"></i>
+                    <span slot="title">Umi子应用</span>
+                </el-menu-item>
+            </el-menu>
+            <div class="collapse-btn" @click="isCollapse = !isCollapse">
+                <i v-if="isCollapse" class="el-icon-d-arrow-right"></i>
+                <i v-else class="el-icon-d-arrow-left"></i>
+            </div>
+        </div>
+        <div class="right-wrap">
+            <router-view id="MainAppArea" v-if="$route.name === 'Main'" />
 
-                <div else id="MicroAppArea"></div>
-            </el-col>
-        </el-row>
+            <div else id="MicroAppArea"></div>
+        </div>
     </div>
 </template>
 
@@ -49,6 +52,11 @@
 import start from '@/micro'
 import { startOption } from "@/micro/config.js";
 export default {
+    data() {
+        return {
+            isCollapse: false
+        }
+    },
     computed: {
         activeRoute() {
             return this.$route.path.split('/')[1]
@@ -73,14 +81,28 @@ export default {
 @import url("../assets/styles/reset.less");
 .home-page {
     height: 100vh;
+    width: 100vw;
     overflow: hidden;
-    .tac {
+    display: flex;
+    .left-wrap {
         height: 100%;
+        // width: 180px;
+        position: relative;
+        .collapse-btn {
+            position: absolute;
+            right: 0;
+            bottom: 50%;
+            cursor: pointer;
+            color: #ffd04b;
+            font-size: 24px;
+        }
+    }
+    .right-wrap {
+        height: 100%;
+        min-width: calc(100% - 64px);
     }
     .el-menu {
         border-right: 0;
-    }
-    .el-col,.el-menu {
         height: 100%;
     }
 }
